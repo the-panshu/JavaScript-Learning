79151612b7170e29dba4c96f5e596bcffe61dc3b
Deepanshu Rajput <rajputdeepanshuxyz1@gmail.com>
2023-11-05 18:04:18 +0000

    We Learn about "Variable" , "const" , "let"

---
+++ 01_Basics/01.Variable.js
@@ -0,0 +1,21 @@
+const Number=21
+var Name="Deepanshu"
+let Email="rajdeep@gmail.com"
+City="Kanpur"
+let Academy; //Value is not defined then it store it with "Undefined"
+
+//Number=22 
+// NUmber is Constant So we can not change it
+
+//Printing using Console.log
+console.log(Number)
+console.log(Name)
+console.log(Email)
+
+// Better Approach to use Console in One Go....
+console.table([Number , Name , Email])
+
+/*
+Please Do not take use of Var in daily life...
+Use one "const" and "let"...
+*/
