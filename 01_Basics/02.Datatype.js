bebd6262904d4b3525cbdd3044b98e8d4c268177
Deepanshu Rajput <rajputdeepanshuxyz1@gmail.com>
2023-11-05 18:26:17 +0000

    We learnt About Datatypes

---
+++ 01_Basics/02.Datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //Used to tell that I'm gonna use newer Version of JS code
+
+
+// alert("Hello") //Used for Window-Pop Up
+// Upper line will not execute because we are using Node not Browser...
+
+//CODE READABILITY IS VERY IMPORTANT...
+
+//Different Type of DataTypes...
+//number => 2 to the Power 53
+//bigint
+//boolean
+//string 
+//null => Standalone Value ==> Yehh ek Object hai :) iska type...
+//undefined 
+//symbol => uniqueness
+
+
+//Object
+//To know the type
+console.log(typeof "Deepanshu")
