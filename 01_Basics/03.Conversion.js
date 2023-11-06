ea51812a56f31f15e24d092b4de0ac055313eadf
Deepanshu Rajput <rajputdeepanshuxyz1@gmail.com>
2023-11-06 11:26:38 +0000

    Conversion of Datatypes

---
+++ 01_Basics/03.Conversion.js
@@ -0,0 +1,7 @@
+let score="33" 
+let Value=number(score);
+console.log(typeof score)
+// In this case score is string and Value is number...
+// If 33---> 33abc hota to Output of Value will be "NaN"=> Not a Number
+
+
